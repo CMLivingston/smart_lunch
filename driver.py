9f7a85be5bd1cb73c82f0cf537290693f13d4917
Chris Livingston <clivingston45@gmail.com>
2015-08-28 14:03:05 -0400

    First Commit-driver/graphics/README

---
+++ driver.py
@@ -0,0 +1,36 @@
+## Main Driver Module for Simulation of Dartmouth Lunch Hour
+## WITH and WITHOUT the INTERNET OF THINGS
+##
+## Created by Chris Livingston and Jasper Bingham
+##
+##
+##
+## IMPORTS
+from graphics import *
+##
+##
+##
+
+
+def main():
+    
+    # create a graphics window for the simulation and for the objects to be drawn on   
+    win = GraphWin("IoT Simulation", 858, 638)
+    background_image = "./static/dart.gif"
+    dartMap(background_image, win)
+    
+    # to end sim
+    win.getMouse()
+    win.close()
+    
+ 
+# draws Dartmouth map into provided graphics window
+def dartMap(im, win):
+    p = Point(434, 320)
+    i = Image(p,im)
+    i.draw(win)
+
+
+
+main()
+
