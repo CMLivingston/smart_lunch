26566e034fc7c86daa72f6ce57c4ec1d82c6f6f1
Jasper K. Bingham <jbingham@cs.dartmouth.edu>
2015-08-29 14:45:41 -0400

    made student methods

--- load_data.py
+++ load_data.py
@@ -19,7 +19,7 @@ def make_foods(filepath):
         # make an array of strings seperated by ,
         x = line.strip()
         s = x.split(',')
-        f = Food(s[0], s[1])
+        f = Food(s[0], float(s[1]))
         foods.append(f)
 
     return foods
@@ -39,7 +39,7 @@ def make_stations(filepath, foods):
         station_name = s[0]
         for food in foods:
             if station_name == food.name:
-                station = Station(s[0], s[1], s[2], food)
+                station = Station(s[0], int(s[1]), int(s[2]), food)
                 if s[3] == "hop":
                     hop_stations.append(station)
                 elif s[3] == "collis":
@@ -64,7 +64,7 @@ def make_venues(filepath, stations):
         x = line.strip()
         s = x.split(',')
         # construct a veune for placement into dict
-        v = Venue(s[0],s[1],s[2],s[3],s[4],stations[s[5]])
+        v = Venue(s[0],int(s[1]),int(s[2]),float(s[3]),int(s[4]),stations[s[5]])
       
         # put name string as index of dict
         venues[s[0]] = v
@@ -88,12 +88,72 @@ def make_classrooms(filepath):
     
     return classrooms
 
+def make_student(filepath, idnum, classroom, venues):
+    in_file = open(filepath, 'r')
+    l = in_file.readline()
+    x = l.strip()
+    s = x.split(',')
+    preferences = make_prefs(venues, idnum)
+    times = []
+    stud = Student(idnum, classroom.x, classroom.y, int(s[0]), int(s[1]), preferences, times, False)
+    return stud
+
+def make_prefs(venues, idnum):
+    # first we need to rank the student's venue choices
+    ret = [[0 for x in range(2)] for x in range(len(venues))]
+    curr_rank = 0  # keeps track of which rank we're dealing with
+    venues_copy = []
+    for venue in venues:
+        venues_copy.append(venues[venue])
+    for y in range(0, len(venues_copy)):
+        total_prob = 0  # will be upper range of our random num generation
+        for venue in venues_copy:
+            total_prob = total_prob + venue.chance
+        bound_matrix = [[0 for z in range(2)] for z in range(len(venues_copy))]
+        curr_bound = 0  # keeps track of which boundary we're making
+        for a in range(0, len(venues_copy)):
+            curr_bound = curr_bound + venues_copy[a].chance
+            bound_matrix[a][0] = venues_copy[a]
+            bound_matrix[a][1] = curr_bound
+        pref = random.uniform(0, total_prob)  # randomly generated number
+        bottom_bound = 0
+        for b in range(0, len(venues_copy)):
+            if pref >= bottom_bound and pref <= bound_matrix[b][1]:
+                ret[curr_rank][0] = venues_copy[b]
+                venues_copy.remove(venues_copy[b])
+                curr_rank = curr_rank + 1
+                break
+            else:
+                bottom_bound = bound_matrix[b][1]
+
+    # then we need to figure out what food they will want at each place
+    for z in range(0, len(venues)):
+        if len(ret[z][0].stations) > 0:  # i.e., not novack
+            curr_bound = 0
+            pref = random.random()
+            for station in ret[z][0].stations:
+                if pref >= curr_bound and pref <= curr_bound + station.food.chance:
+                    ret[z][1] = station
+                    break
+                else:
+                    curr_bound = curr_bound + station.food.chance
+        else:
+            continue
+    return ret
+
 def test():
    
     foods = make_foods("./static/foods.txt")
     stations = make_stations("./static/stations.txt", foods)
     venues = make_venues("./static/venues.txt", stations)
     classrooms = make_classrooms("./static/classrooms.txt")
+    stud_id = 0
+    students = []
+    # make ten sample students
+    while stud_id < 10:
+        s = make_student("./static/student.txt", stud_id, classrooms["wilder hall"], venues)
+        students.append(s)
+        stud_id = stud_id + 1
     print "FOODS:"
     for food in foods:
         print food.name, food.chance
@@ -109,6 +169,13 @@ def test():
     print "CLASSROOMS:"
     for classroom in classrooms:
         print classrooms[classroom], classrooms[classroom].x, classrooms[classroom].y
+    for student in students:
+        print "Student " + str(student.id)
+        for preference in student.preferences:
+            if preference[1] == 0:
+                print preference[0].name, ":", "prepared"
+            else:
+                print preference[0].name, ":", preference[1]
             
 if __name__ == "__main__":
     test()
